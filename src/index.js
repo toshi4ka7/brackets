9172a55709d3f845f9a862b0c52ee2d682a63b9a
Anton <toshi4ka7@mail.ru>
2020-10-06 21:43:02 +0300

    feat: readt task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strReg = '';
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+
+    if (bracketsConfig[i][0] === '(' ||
+        bracketsConfig[i][0] === ')' ||
+        bracketsConfig[i][0] === '[' ||
+        bracketsConfig[i][0] === ']' ||
+        bracketsConfig[i][0] === '|') {
+      strReg += "(\\" + bracketsConfig[i][0];
+    } else {
+      strReg +=  '(' + bracketsConfig[i][0];
+    }
+
+    if (bracketsConfig[i][1] === '(' ||
+        bracketsConfig[i][1] === ')' ||
+        bracketsConfig[i][1] === '[' ||
+        bracketsConfig[i][1] === ']' ||
+        bracketsConfig[i][1] === '|') {
+      strReg += "\\" + bracketsConfig[i][1] + ')|';
+    } else {
+      strReg +=  bracketsConfig[i][1] + ')|';
+    }
+
+  }
+
+
+  strReg = strReg.substring(0, strReg.length - 1);
+  
+ 
+  strReg = new RegExp(strReg, 'g');
+
+
+  while (str.match(strReg) !== null) {
+      str = str.replace(strReg, '');
+  }
+
+  if (str.length !== 0) {
+    return false;
+  } else {
+    return true;
+  }
 }
